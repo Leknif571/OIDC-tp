1b8256a27f3f5dbc2da7b8916afab70b4823a144
Boukada Adel <65848859+Leknif571@users.noreply.github.com>
2026-09-21 11:54:58 +0200

    init project

--- README.md
+++ README.md
@@ -7,6 +7,21 @@ Loan Keovilay
 Valentin Mignon
 Adel Boukada
 
+
+
 Start projet :
 
+Start le container docker de keycloak avec la commande :
+
+```
+docker run -p 8080:8080 -e KC_BOOTSTRAP_ADMIN_USERNAME=admin -e KC_BOOTSTRAP_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:latest start-dev
+```
+
+Configurer keycloak en créant un realm et un client OIDC.
+
+Créer un fichier .env à la racine du projet en vous référant à l'exemple .env-exemple
+
+Lancer le serveur avec la commande :
 node server.js
+
+Rendez-vous sur http://localhost:3000 pour accéder à l'application.
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+const session = require('express-session');
+const crypto = require('node:crypto');
 const express = require('express');
+
+const well_known_url = process.env.WELL_KNOWN_URL;
+let issuer, authorization_endpoint, token_endpoint, userinfo_endpoint;
+
 const app = express();
 const port = 3000;
+
+async function getInfo() {
+    let data = await (await fetch(well_known_url)).json();
+    issuer = data.issuer;
+    authorization_endpoint = data.authorization_endpoint;
+    token_endpoint = data.token_endpoint;
+    userinfo_endpoint = data.userinfo_endpoint;
+
+    console.log(userinfo_endpoint);
+}
+
+getInfo();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -9,3 +27,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Serveur en écoute sur http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
+
